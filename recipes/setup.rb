875de8e6b031b44544b82c34bcf86446179c98fb
prakashapurv <1986.apurv@gmail.com>
2017-03-27 09:48:29 +0000

    Ohio commands

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,6 +1,10 @@
-package 'nano'
+package 'nano' do
+	action :remove
+end
 
-package 'ntp'
+package 'ntp' do
+	action :remove
+end
 
 package 'git' do
     action :install
@@ -8,7 +12,12 @@ end
 
 file '/etc/motd' do
      action :create
-     content 'This is the message of the day \n'
+     content "This is the message of the day
+     Hostname : #{node['hostname']}
+     IPAddress: #{node['ipaddress']}
+     CPU :      #{node['cpu']['0']['mhz']} MHZ
+     Memory :   #{node['memory']['total']}
+"
      mode '0755'
      owner 'vagrant'
 end
